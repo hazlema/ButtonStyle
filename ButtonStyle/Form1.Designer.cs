e3ad4e6fd1f8bf69df9449492ac3b208c3226a2b
hazlema <hazlema@gmail.com>
2016-06-18 17:22:01 -0400

    Add project files.

---
+++ ButtonStyle/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ButtonStyle {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.rounded4 = new ButtonStyle.Rounded();
+            this.rounded3 = new ButtonStyle.Rounded();
+            this.rounded2 = new ButtonStyle.Rounded();
+            this.rounded1 = new ButtonStyle.Rounded();
+            this.SuspendLayout();
+            // 
+            // rounded4
+            // 
+            this.rounded4.BackColor = System.Drawing.Color.Transparent;
+            this.rounded4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rounded4.BackgroundImage")));
+            this.rounded4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rounded4.FlatAppearance.BorderSize = 0;
+            this.rounded4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.rounded4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.rounded4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rounded4.ForeColor = System.Drawing.Color.White;
+            this.rounded4.GradientEnd = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.rounded4.HoverGradientStart = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(90)))), ((int)(((byte)(80)))));
+            this.rounded4.Location = new System.Drawing.Point(152, 12);
+            this.rounded4.Name = "rounded4";
+            this.rounded4.NormalGradientStart = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.rounded4.Rounding = 10;
+            this.rounded4.Size = new System.Drawing.Size(134, 45);
+            this.rounded4.TabIndex = 3;
+            this.rounded4.Text = "rounded4";
+            this.rounded4.UseVisualStyleBackColor = false;
+            // 
+            // rounded3
+            // 
+            this.rounded3.BackColor = System.Drawing.Color.Transparent;
+            this.rounded3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rounded3.BackgroundImage")));
+            this.rounded3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rounded3.FlatAppearance.BorderSize = 0;
+            this.rounded3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.rounded3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.rounded3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rounded3.ForeColor = System.Drawing.Color.White;
+            this.rounded3.GradientEnd = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.rounded3.HoverGradientStart = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(90)))), ((int)(((byte)(80)))));
+            this.rounded3.Location = new System.Drawing.Point(152, 63);
+            this.rounded3.Name = "rounded3";
+            this.rounded3.NormalGradientStart = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.rounded3.Rounding = 10;
+            this.rounded3.Size = new System.Drawing.Size(134, 45);
+            this.rounded3.TabIndex = 2;
+            this.rounded3.Text = "rounded3";
+            this.rounded3.UseVisualStyleBackColor = false;
+            // 
+            // rounded2
+            // 
+            this.rounded2.BackColor = System.Drawing.Color.Transparent;
+            this.rounded2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rounded2.BackgroundImage")));
+            this.rounded2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rounded2.FlatAppearance.BorderSize = 0;
+            this.rounded2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.rounded2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.rounded2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rounded2.ForeColor = System.Drawing.Color.White;
+            this.rounded2.GradientEnd = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.rounded2.HoverGradientStart = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(90)))), ((int)(((byte)(80)))));
+            this.rounded2.Location = new System.Drawing.Point(12, 63);
+            this.rounded2.Name = "rounded2";
+            this.rounded2.NormalGradientStart = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.rounded2.Rounding = 10;
+            this.rounded2.Size = new System.Drawing.Size(134, 45);
+            this.rounded2.TabIndex = 1;
+            this.rounded2.Text = "rounded2";
+            this.rounded2.UseVisualStyleBackColor = false;
+            // 
+            // rounded1
+            // 
+            this.rounded1.BackColor = System.Drawing.Color.Transparent;
+            this.rounded1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rounded1.BackgroundImage")));
+            this.rounded1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rounded1.FlatAppearance.BorderSize = 0;
+            this.rounded1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.rounded1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.rounded1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rounded1.ForeColor = System.Drawing.Color.White;
+            this.rounded1.GradientEnd = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.rounded1.HoverGradientStart = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(90)))), ((int)(((byte)(80)))));
+            this.rounded1.Location = new System.Drawing.Point(12, 12);
+            this.rounded1.Name = "rounded1";
+            this.rounded1.NormalGradientStart = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.rounded1.Rounding = 10;
+            this.rounded1.Size = new System.Drawing.Size(134, 45);
+            this.rounded1.TabIndex = 0;
+            this.rounded1.Text = "rounded1";
+            this.rounded1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(297, 119);
+            this.Controls.Add(this.rounded4);
+            this.Controls.Add(this.rounded3);
+            this.Controls.Add(this.rounded2);
+            this.Controls.Add(this.rounded1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Rounded rounded1;
+        private Rounded rounded2;
+        private Rounded rounded3;
+        private Rounded rounded4;
+    }
+}
+
---
+++ ButtonStyle/Rounded.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace ButtonStyle {
+    class Rounded : Button {
+        private Bitmap[] Background;
+
+        public Rounded() {
+            this.BackColor = Color.Transparent;
+            this.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            this.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            this.FlatStyle = FlatStyle.Flat;
+            this.FlatAppearance.BorderSize = 0;
+            this.ForeColor = Color.White;
+            this.Cursor = Cursors.Hand;
+        }
+
+        public int Rounding { get; set; } = 10;
+        public Color HoverGradientStart { get; set; } = Color.FromArgb(255, 90, 90, 80);
+        public Color NormalGradientStart { get; set; } = Color.FromArgb(255, 60, 60, 60);
+        public Color GradientEnd { get; set; } = Color.FromArgb(255, 20, 20, 20);
+
+        private void PrePaint(Graphics g, Color[] GradEnd, Color GradStart) {
+            using (GraphicsPath Path = new GraphicsPath()) {
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+
+                // Points
+                Rectangle[] Corners = new Rectangle[] {
+                new Rectangle(0, 0, Rounding, Rounding),
+                new Rectangle(Width - Rounding, 0, Rounding, Rounding),
+                new Rectangle(Width - Rounding, Height - Rounding, Rounding, Rounding),
+                new Rectangle(0, Height - Rounding, Rounding, Rounding)
+            };
+
+                Point[] Lines = new Point[] {
+                new Point(Rounding, 0), new Point(Width - Rounding, 0),
+                new Point(Width, Rounding), new Point(Width, Height - Rounding),
+                new Point(Width - Rounding, Height), new Point(Rounding, Height)
+            };
+
+                // Create Path
+                Path.StartFigure();
+                Path.AddArc(Corners[0], 180, 90);
+                Path.AddLine(Lines[0], Lines[1]);
+                Path.AddArc(Corners[1], -90, 90);
+                Path.AddLine(Lines[2], Lines[3]);
+                Path.AddArc(Corners[2], 0, 90);
+                Path.AddLine(Lines[4], Lines[5]);
+                Path.AddArc(Corners[3], 90, 90);
+                Path.CloseFigure();
+
+                // Gradient
+                PathGradientBrush GBrush = new PathGradientBrush(Path);
+                GBrush.CenterColor = GradStart;
+                GBrush.SurroundColors = GradEnd;
+                GBrush.CenterPoint = new Point(Width, Height);
+
+                g.FillPath(GBrush, Path);
+            }
+        }
+
+        public void buildButtonBackgrounds() {
+            // Init Backgrounds
+            Background = new Bitmap[] {
+                new Bitmap(Width, Height, PixelFormat.Format32bppPArgb),
+                new Bitmap(Width, Height, PixelFormat.Format32bppPArgb)
+            };
+
+            // Normal
+            using (Graphics g = Graphics.FromImage(Background[0])) {
+                PrePaint(g, 
+                    new Color[] { NormalGradientStart },
+                    GradientEnd
+                );
+
+            }
+
+            // Hover
+            using (Graphics g = Graphics.FromImage(Background[1])) {
+                PrePaint(g,
+                    new Color[] { HoverGradientStart },
+                    GradientEnd
+                );
+            }
+
+            this.BackgroundImage = Background[0];
+        }
+
+        protected override void OnMouseLeave(EventArgs e) {
+            if (Background[0]?.Size != Size)
+                buildButtonBackgrounds();
+
+            this.BackgroundImage = Background[0];
+            base.OnMouseLeave(e);
+        }
+
+        protected override void OnMouseEnter(EventArgs e) {
+            if (Background[1]?.Size != Size)
+                buildButtonBackgrounds();
+
+            this.BackgroundImage = Background[1];
+            base.OnMouseEnter(e);
+        }
+
+        protected override void OnParentVisibleChanged(EventArgs e) {
+            buildButtonBackgrounds();
+            OnMouseLeave(e);
+            base.OnParentVisibleChanged(e);
+        }
+
+        protected override void OnResize(EventArgs e) {
+            buildButtonBackgrounds();
+            base.OnResize(e);
+        }
+    }
+}
